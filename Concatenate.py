cf8cf38ccfe9973ffc86a3fcf07d2f720bdfc7e5
Alexander Kuznetsov <71138865+AlexKu4@users.noreply.github.com>
2024-06-09 20:55:25 +0300

    Fix bugs

--- Concatenate.py
+++ Concatenate.py
@@ -12,7 +12,7 @@ def devide_nums(num1, num2):
     else:
         return -1
 def subtracting_nums(num1, num2):
-    if if isinstance(num1, int) and isinstance(num2, int):
+    if isinstance(num1, int) and isinstance(num2, int):
         return num1 - num2
     else:
         return -1
